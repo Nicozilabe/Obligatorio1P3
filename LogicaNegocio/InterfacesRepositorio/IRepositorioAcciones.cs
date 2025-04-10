1107351d5d5126917325b6be013bc26654792fb2
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-10 18:36:04 -0300

    Commit inicial.

---
+++ LogicaNegocio/InterfacesRepositorio/IRepositorioAcciones.cs
@@ -0,0 +1,13 @@
+﻿using LogicaNegocio.EntidadesDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.InterfacesRepositorio
+{
+    public interface IRepositorioAcciones:IRepositorio<Accion>
+    {
+    }
+}
