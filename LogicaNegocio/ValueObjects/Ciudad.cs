dba0edf6cff2d8c9a449d9c084627c636568ec3d
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-11 17:38:44 -0300

    Implementada Clase Ciudad

---
+++ LogicaNegocio/ValueObjects/Ciudad.cs
@@ -0,0 +1,31 @@
+﻿using ExcepcionesPropias;
+using LogicaNegocio.InterfacesDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.ValueObjects
+{
+    public record Ciudad: IValidable
+    {
+        public string Nombre { get; init; }
+
+        public Ciudad(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public void Validar() {
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                throw new DatosInvalidosException("Nombre-Ciudad no válido");
+            }
+            if(Nombre.Length > 32)
+            {
+                throw new DatosInvalidosException("Nombre-Ciudad debe ser un string de 1 a 32 caracteres");
+            }
+        }
+    }
+}
--- LogicaNegocio/ValueObjects/DireccionPostal.cs
+++ LogicaNegocio/ValueObjects/DireccionPostal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExcepcionesPropias;
+using LogicaNegocio.Enums;
+using LogicaNegocio.InterfacesDominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,33 @@ using System.Threading.Tasks;
 
 namespace LogicaNegocio.ValueObjects
 {
-    public record DireccionPostal
+    public record DireccionPostal : IValidable
     {
+
+        public string Calle { get; init; }
+        public int Numero { get; init; }
+        public TipoCiudad Ciudad { get; init; }
+        public int CodigoPostal { get; init; }
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Calle)) {
+                throw new DatosInvalidosException("Calle debe ser un valor válido");
+            }
+            if(Calle.Length > 32)
+            {
+                throw new DatosInvalidosException("Calle no debe contener ente 1 y 32 caracteres.");
+            }
+            if (Numero <= 0 || Numero > 9999 || Numero == null) {
+                throw new DatosInvalidosException("Número-Dirección Debe ser un valor entre 1 y 9999");
+            }
+            if (CodigoPostal <= 0 || CodigoPostal > 99999 || CodigoPostal == null)
+            {
+                throw new DatosInvalidosException("Número-Dirección Debe ser un valor entre 1 y 99999");
+            }
+            if(Ciudad == null)
+            {
+                throw new DatosInvalidosException("Ciudad no válida");
+            }
+        }
     }
 }
