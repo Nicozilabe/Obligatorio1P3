e19539d9855a78aef22d174969d22a138ab6af8a
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-11 17:47:22 -0300

    Creación valores VO Ubicación

--- LogicaNegocio/ValueObjects/Ubicacion.cs
+++ LogicaNegocio/ValueObjects/Ubicacion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogicaNegocio.InterfacesDominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace LogicaNegocio.ValueObjects
 {
-    public record Ubicacion
+    public record Ubicacion:IValidable
     {
+        public double Latitud { get; init; }
+        public double Longitud { get; init; }
+
+        public void Validar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
