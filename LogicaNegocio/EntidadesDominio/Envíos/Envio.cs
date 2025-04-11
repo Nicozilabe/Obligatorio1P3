0e8e4a33bbc6c1153a49fa18de7e5e4a7bf565a6
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-11 18:18:51 -0300

    Creadas propiedades Envío

--- LogicaNegocio/EntidadesDominio/Envíos/Envio.cs
+++ LogicaNegocio/EntidadesDominio/Envíos/Envio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LogicaNegocio.EntidadesDominio.Usuarios;
+using LogicaNegocio.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,12 @@ namespace LogicaNegocio.EntidadesDominio.Envíos
 {
     public abstract class Envio
     {
+        public int Id { get; set; }
+        public int Tracking { get; set; }
+        public Empleado EmpleadoResponable { get; set; }
+        public Cliente Cliente { get; set; }
+        public double Peso { get; set; }
+        public TipoEstadoEnvio EstadoEnvio { get; set; }
+        public TipoSeguimiento Seguimiento { get; set; }
     }
 }
