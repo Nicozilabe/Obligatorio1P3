dfa493bca2808433b3545665c0b37417c2d750e6
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-11 18:42:52 -0300

    Implementada Class EnvíoUrgente

--- LogicaNegocio/EntidadesDominio/Envíos/EnvioUrgente.cs
+++ LogicaNegocio/EntidadesDominio/Envíos/EnvioUrgente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogicaNegocio.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace LogicaNegocio.EntidadesDominio.Envíos
 {
     public class EnvioUrgente:Envio
     {
+        public DireccionPostal Direccion { get; set; }
+        public bool EnvioEficiente { get; set; }
+
+        public EnvioUrgente() { }
+
+        public EnvioUrgente(DireccionPostal direccion)
+        {
+            Direccion = direccion;
+
+        }
     }
 }
