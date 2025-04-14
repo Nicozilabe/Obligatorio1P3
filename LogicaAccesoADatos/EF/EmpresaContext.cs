950d9c72ec50bbe55a20e93ca08c820388527192
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-14 19:36:20 -0300

    Cambio a Configuración Conection String vía inyección de depenendenicas

--- LogicaAccesoADatos/EF/EmpresaContext.cs
+++ LogicaAccesoADatos/EF/EmpresaContext.cs
@@ -13,12 +13,6 @@ namespace LogicaAccesoADatos.EF
         public DbSet<Usuario> Usuarios { get; set; }
 
         public EmpresaContext(DbContextOptions options): base(options) { }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(
-                @"SERVER=(localdb)\MSSQLLocalDB; Initial Catalog = EmpresaV1; Integrated Security = True;");
-
-        }
+        
     }
 }
