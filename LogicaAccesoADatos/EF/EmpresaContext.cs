2167fe11b93bf6b711f61f99b4edb174c3e76010
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-14 19:15:05 -0300

    Configurada Conexión servidor sql EmpresaContext

--- LogicaAccesoADatos/EF/EmpresaContext.cs
+++ LogicaAccesoADatos/EF/EmpresaContext.cs
@@ -11,5 +11,13 @@ namespace LogicaAccesoADatos.EF
     public class EmpresaContext:DbContext
     {
         public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(
+                @"SERVER=(localdb)\MSSQLLocalDB; Initial Catalog = EmpresaV1; Integrated Security = True;");
+
+        }
     }
 }
