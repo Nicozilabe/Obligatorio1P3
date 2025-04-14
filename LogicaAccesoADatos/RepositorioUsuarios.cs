1a82244e45f25de5ef10b3f339e1ecfd6c4bf147
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-14 18:34:04 -0300

    Creada Class RepositorioUsuarios

---
+++ LogicaAccesoADatos/RepositorioUsuarios.cs
@@ -0,0 +1,43 @@
+﻿using LogicaNegocio.EntidadesDominio.Usuarios;
+using LogicaNegocio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoADatos
+{
+    public class RepositorioUsuarios : IRepositorioUsuarios
+    {
+        public void Add(Usuario obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario FindByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
