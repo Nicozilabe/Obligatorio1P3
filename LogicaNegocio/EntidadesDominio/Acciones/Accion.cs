469dee37f6121e17c2724e22157005d574d59953
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-15 18:37:02 -0300

    idem

---
+++ LogicaNegocio/EntidadesDominio/Acciones/Accion.cs
@@ -0,0 +1,26 @@
+﻿using LogicaNegocio.EntidadesDominio.Usuarios;
+using LogicaNegocio.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.EntidadesDominio.Acciones
+{
+    public abstract class Accion
+    {
+        public int Id { get; set; }
+        public Empleado Realizador { get; set; }
+        public FechaAccion Fecha { get; set; }
+
+        public Accion() { }
+
+        public Accion( Empleado realizador, FechaAccion fecha)
+        {
+          
+            Realizador = realizador;
+            Fecha = fecha;
+        }
+    }
+}
---
+++ LogicaNegocio/EntidadesDominio/Acciones/AccionEnvio.cs
@@ -0,0 +1,28 @@
+﻿using LogicaNegocio.EntidadesDominio.Envíos;
+using LogicaNegocio.EntidadesDominio.Usuarios;
+using LogicaNegocio.Enums;
+using LogicaNegocio.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.EntidadesDominio.Acciones
+{
+    public class AccionEnvio : Accion
+    {
+        public TipoAccionEnvio TipoAccion { get; set; }
+        public Envio Envio { get; set; }
+        public TipoComentarioEnvio ComentarioEnvio { get; set; }
+
+        public AccionEnvio() { }
+
+        public AccionEnvio(TipoAccionEnvio tipoAccion, Envio envio, TipoComentarioEnvio comentarioEnvio, Empleado realizador, FechaAccion fecha):base(realizador, fecha)
+        {
+            TipoAccion = tipoAccion;
+            Envio = envio;
+            ComentarioEnvio = comentarioEnvio;
+        }
+    }
+}
