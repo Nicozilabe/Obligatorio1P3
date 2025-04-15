db3518c52990a1e2722f0d2bcdb3e7bb5b72d5f4
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-15 18:42:39 -0300

    Implementada AcciónAdministración

--- LogicaNegocio/EntidadesDominio/Acciones/AccionAdministracion.cs
+++ LogicaNegocio/EntidadesDominio/Acciones/AccionAdministracion.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LogicaNegocio.EntidadesDominio.Usuarios;
+using LogicaNegocio.Enums;
+using LogicaNegocio.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,17 @@ namespace LogicaNegocio.EntidadesDominio.Acciones
 {
     public class AccionAdministracion : Accion
     {
+        public Empleado Afectado { get; set; }
+        public Administrador Realizador { get; set; }
+        public TipoAccionAdministracion TipoAccion { get; set; }
+
+        public AccionAdministracion() { }
+
+        public AccionAdministracion(Empleado afectado, Administrador realizador, TipoAccionAdministracion tipoAccion, FechaAccion fecha):base(fecha)
+        {
+            Afectado = afectado;
+            Realizador = realizador;
+            TipoAccion = tipoAccion;
+        }
     }
 }
