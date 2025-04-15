49d2b81c942a237d25b4e78b6868788fe3bd8830
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-04-15 15:30:31 -0300

    Agregado Repositorio Usuarios

---
+++ LogicaAccesoADatos/Repos/RepositorioUsuarios.cs
@@ -0,0 +1,71 @@
+﻿using ExcepcionesPropias;
+using LogicaAccesoADatos.EF;
+using LogicaNegocio.EntidadesDominio.Usuarios;
+using LogicaNegocio.InterfacesRepositorio;
+using LogicaNegocio.ValueObjects.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoADatos.Repos
+{
+    public class RepositorioUsuarios : IRepositorioUsuarios
+    {
+
+        public EmpresaContext Context { get; set; }
+
+        public RepositorioUsuarios(EmpresaContext context)
+        {
+
+            Context = context;
+
+        }
+
+        public void Add(Usuario obj)
+        {
+            if (obj == null)
+            {
+                throw new DatosInvalidosException("Usuario no válido para el alta.");
+            }
+            obj.Validar();
+
+            Usuario buscado = null;
+
+            if (buscado != null)
+            {
+                throw new DatosInvalidosException("El Usuario ya existe");
+            }
+            Context.Usuarios.Add(obj);
+            Context.SaveChanges();
+        }
+
+        public List<Usuario> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario FindByEmail(string email)
+        {
+            Usuario? buscado = Context.Usuarios.Where(Usuario => Usuario.Email.Email == email).SingleOrDefault();
+            return buscado;
+        }
+
+        public Usuario FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario obj)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
