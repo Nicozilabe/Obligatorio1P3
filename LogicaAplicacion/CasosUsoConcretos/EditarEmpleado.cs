834b3caa96d3dff7090cadff8d605497810c25ba
AnisKiki <nadyrmiguel77@gmail.com>
2025-04-26 19:40:15 -0300

    Implementacion interface Editar empleado.

---
+++ LogicaAplicacion/CasosUsoConcretos/EditarEmpleado.cs
@@ -0,0 +1,18 @@
+﻿using CasosDeUso.DTOs;
+using CasosDeUso.InterfacesCasosUso;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUsoConcretos
+{
+    public class EditarEmpleado : IEditarEmpleado
+    {
+        void IEditarEmpleado.EditarEmpleado(UsuarioDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
