a125886c6435e2ccb7faf3e0998c3d83c1359164
AnisKiki <nadyrmiguel77@gmail.com>
2025-04-27 16:19:41 -0300

    Implementacion de IValidable en Empleado y Usuario dto

--- CasosDeUso/DTOs/EmpleadoDTO.cs
+++ CasosDeUso/DTOs/EmpleadoDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CasosDeUso.DTOs
 {
-    public class EmpleadoDTO
+    public class EmpleadoDTO:IValidable
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
@@ -14,5 +16,37 @@ namespace CasosDeUso.DTOs
         public string Email { get; set; }
         public String Rol { get; set; }
         public bool Activo {  get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                throw new DatosInvalidosException("El nombre no puede quedar vacio.");
+            }
+            if (Nombre.Length > 32)
+            {
+                throw new DatosInvalidosException("El nombre debe tener menos de 32 letras");
+            }
+            if (string.IsNullOrEmpty(Apellido))
+            {
+                throw new DatosInvalidosException("El apellido no puede quedar vacio.");
+            }
+            if (Apellido.Length > 32)
+            {
+                throw new DatosInvalidosException("El apellido debe tener menos de 32 letras");
+            }
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new DatosInvalidosException("El email no puede quedar vacio.");
+            }
+            if (Email.Length > 32)
+            {
+                throw new DatosInvalidosException("El email debe tener menos de 32 letras");
+            }
+            if (Rol != "Empleado" && Rol != "Administrador")
+            {
+                throw new DatosInvalidosException("Tipo Usuario no válido");
+            }
+        }
     }
 }
--- CasosDeUso/DTOs/UsuarioDTO.cs
+++ CasosDeUso/DTOs/UsuarioDTO.cs
@@ -1,4 +1,6 @@
 ﻿using CasosDeUso.Enums;
+using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace CasosDeUso.DTOs
 {
-    public class UsuarioDTO
+    public class UsuarioDTO: IValidable
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
@@ -15,5 +17,37 @@ namespace CasosDeUso.DTOs
         public string Email { get; set; }
         public String Rol { get; set; }
         public bool Activo { get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                throw new DatosInvalidosException("El nombre no puede quedar vacio.");
+            }
+            if (Nombre.Length > 32)
+            {
+                throw new DatosInvalidosException("El nombre debe tener menos de 32 letras");
+            }
+            if (string.IsNullOrEmpty(Apellido))
+            {
+                throw new DatosInvalidosException("El apellido no puede quedar vacio.");
+            }
+            if (Apellido.Length > 32)
+            {
+                throw new DatosInvalidosException("El apellido debe tener menos de 32 letras");
+            }
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new DatosInvalidosException("El email no puede quedar vacio.");
+            }
+            if (Email.Length > 32)
+            {
+                throw new DatosInvalidosException("El email debe tener menos de 32 letras");
+            }
+            if (Rol != "Empleado" && Rol != "Administrador" && Rol != "Cliente")
+            {
+                throw new DatosInvalidosException("Tipo Usuario no válido");
+            }
+        }
     }
 }
