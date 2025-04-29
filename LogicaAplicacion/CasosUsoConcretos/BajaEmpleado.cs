53aa39aa9eb2a9f104a69b6069785848c6eb34b3
AnisKiki <nadyrmiguel77@gmail.com>
2025-04-29 19:31:41 -0300

    Implementacion de BajaEmpleado en casosUsoConcretos.

---
+++ LogicaAplicacion/CasosUsoConcretos/BajaEmpleado.cs
@@ -0,0 +1,27 @@
+﻿using CasosDeUso.DTOs;
+using CasosDeUso.InterfacesCasosUso;
+using LogicaAplicacion.Mapeadores;
+using LogicaNegocio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUsoConcretos
+{
+    public class BajaEmpleado : IBajaEmpleado
+    {
+        public IRepositorioEmpleados repo { get; set; }
+
+        public BajaEmpleado(IRepositorioEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        void IBajaEmpleado.BajaEmpleado(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
