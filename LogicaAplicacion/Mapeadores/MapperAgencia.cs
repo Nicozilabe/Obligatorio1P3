194c903133fa5349fb346835e2e65d350c1416b0
Nicozilabe <nicozilabe@hotmail.com>
2025-05-02 11:45:58 -0300

    MapperAgencia

---
+++ LogicaAplicacion/Mapeadores/MapperAgencia.cs
@@ -0,0 +1,20 @@
+﻿using CasosDeUso.DTOs.Envio;
+using LogicaNegocio.EntidadesDominio.Envíos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.Mapeadores
+{
+    public class MapperAgencia
+    {
+
+        public static List<AgenciaDTO> ToListDTO(List<Agencia> agencias)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
