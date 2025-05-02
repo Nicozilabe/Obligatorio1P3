8c5e1414497acafe96db91b9eaf343a933acb097
Nicozilabe <nicozilabe@hotmail.com>
2025-05-02 11:45:13 -0300

    Agregado RepositorioAgencias

---
+++ LogicaAccesoADatos/Repos/RepositorioAgencias.cs
@@ -0,0 +1,47 @@
+﻿using LogicaAccesoADatos.EF;
+using LogicaNegocio.EntidadesDominio.Envíos;
+using LogicaNegocio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoADatos.Repos
+{
+    public class RepositorioAgencias : IRepositorioAgencias
+    {
+        public EmpresaContext Context { get; set; }
+
+        public RepositorioAgencias(EmpresaContext context)
+        {
+
+            Context = context;
+
+        }
+        public void Add(Agencia obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Agencia> FindAll()
+        {
+            return Context.Agencias.ToList();
+        }
+
+        public Agencia FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Agencia obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
