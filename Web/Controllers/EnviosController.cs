87bc5fc0d9a67b78d57ee2b0de52dd32d61f1e7d
Nicozilabe <nicozilabe@hotmail.com>
2025-05-02 10:25:43 -0300

    Creado enviosController

---
+++ Web/Controllers/EnviosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class EnviosController : Controller
+    {
+       public IActionResult AltaEnvio()
+        {
+            if (HttpContext.Session.GetString("LogeadoRol") == "Administrador")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("NoAutorizado", "Auth");
+            }
+        }
+    }
+}
