af8087224c0a6749eee624a9b235851fc338888a
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-05-03 18:19:44 -0300

    Implementado get AltaEnvio y PostParcialmente

--- Web/Controllers/EnviosController.cs
+++ Web/Controllers/EnviosController.cs
@@ -1,25 +1,88 @@
-﻿using CasosDeUso.InterfacesCasosUso;
+﻿using CasosDeUso.DTOs.Envio;
+using CasosDeUso.InterfacesCasosUso;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class EnviosController : Controller
     {
         public IObtenerAgencias CUObtenerAgencias { get; set; }
+        public IObtenerCiudades CUObtenerCiudades { get; set; }
 
-        public EnviosController(IObtenerAgencias cUObtenerAgencias) {
-            CUObtenerAgencias = cUObtenerAgencias;
-        }
-       public IActionResult AltaEnvio()
+        
+
+
+        public EnviosController(IObtenerAgencias cUObtenerAgencias, IObtenerCiudades cUObtenerCiudades)
         {
-            if (HttpContext.Session.GetString("LogeadoRol") == "Administrador")
+            CUObtenerAgencias = cUObtenerAgencias;
+            CUObtenerCiudades = cUObtenerCiudades;
+        }
+
+
+
+        public IActionResult AltaEnvio()
+        {
+            if (HttpContext.Session.GetString("LogeadoRol") == "Administrador" || true)
             {
-                return View();
+
+                //Evitar carga al servidor, Extraido de ChatGPT
+                IEnumerable<AgenciaDTO> agencias = CUObtenerAgencias.GetAgencias();
+                IEnumerable<CiudadDTO> ciudades = CUObtenerCiudades.GetCiudades();
+
+                TempData["Agencias"] = JsonConvert.SerializeObject(agencias);
+                TempData["Ciudades"] = JsonConvert.SerializeObject(ciudades);
+
+                TempData.Keep("Agencias");
+                TempData.Keep("Ciudades");
+
+
+                RegistroEnvioViewModel model = new RegistroEnvioViewModel
+                {
+                    direccion = new DireccionDTO(),
+                    Agencias = agencias,
+                    Ciudades = ciudades,
+                    EmailCliente = "",
+                    Peso = 0,
+                    TipoEnvio = "",
+                    IdAgencia = 0
+                };
+
+                return View(model);
             }
             else
             {
                 return RedirectToAction("NoAutorizado", "Auth");
             }
+        }
+        [HttpPost]
+        public IActionResult AltaEnvio(RegistroEnvioViewModel model)
+        {
+            //Evitar carga al servidor, Extraido de ChatGPT
+            var agenciasJson = TempData["Agencias"]?.ToString();
+            var ciudadesJson = TempData["Ciudades"]?.ToString();
+
+            var agencias = JsonConvert.DeserializeObject<List<AgenciaDTO>>(agenciasJson);
+            var ciudades = JsonConvert.DeserializeObject<List<CiudadDTO>>(ciudadesJson);
+
+
+
+            AgenciaDTO a = agencias.FirstOrDefault(x => x.Id == model.IdAgencia);
+            CiudadDTO c = ciudades.FirstOrDefault(x => x.Id == model.IdCiudad);
+            model.direccion.Ciudad = c;
+
+            RegistroEnvioDTO reg = new RegistroEnvioDTO
+            {
+                IdEmpleadoResponable = 1 /*int.Parse(HttpContext.Session.GetString("LogeadoId"))*/,
+                EmailCliente = model.EmailCliente,
+                Peso = model.Peso,
+                TipoEnvio = model.TipoEnvio,
+                Agencia = a,
+                direccion = model.direccion
+            };
+            
+            return View();
         }
     }
 }
