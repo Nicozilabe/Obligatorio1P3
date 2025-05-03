b75a7552514940f379b749bef03d9e7fb7c4c98b
AnisKiki <nadyrmiguel77@gmail.com>
2025-05-03 19:39:28 -0300

    Implementacion Validacion DtoLogin

--- CasosDeUso/DTOs/Usuarios/LoginDTO.cs
+++ CasosDeUso/DTOs/Usuarios/LoginDTO.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CasosDeUso.DTOs.Usuarios
 {
-    public class LoginDTO
+    public class LoginDTO : IValidable
     {
         public string Email { get; set; }
         public string Pass { get; set; }
+
+        public void Validar()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new DatosInvalidosException("El Email no puede quedar vacio.");
+            }
+            if (string.IsNullOrEmpty(Pass))
+            {
+                throw new DatosInvalidosException("La contraseña no puede quedar vacio.");
+            }
+        }
     }
 }
