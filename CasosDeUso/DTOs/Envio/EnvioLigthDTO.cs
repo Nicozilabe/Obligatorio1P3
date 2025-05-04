a69e75cd6d1481fd0ec0c78ba30132b443ccdbe3
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-05-03 23:54:25 -0300

    Creado envíoLight dto para no cargar a la base

---
+++ CasosDeUso/DTOs/Envio/EnvioLigthDTO.cs
@@ -0,0 +1,20 @@
+﻿using CasosDeUso.DTOs.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasosDeUso.DTOs.Envio
+{
+    public class EnvioLigthDTO
+    {
+        public int Id { get; set; }
+        public double Peso { get; set; }
+        public string TipoEnvio { get; set; }
+        public string EstadoEnvio { get; set; }
+        public EmpleadoDTO Empleado { get; set; }
+        public string EmailCliente { get; set; }
+        public string Destino { get; set; }
+    }
+}
