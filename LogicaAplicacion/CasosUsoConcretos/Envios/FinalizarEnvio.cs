dee8622b1e930e1c3849735255d97659aeabf09e
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-05-12 19:42:05 -0300

    Añadir clase FinalizarEnvio con validaciones y lógica
    Se ha añadido la clase `FinalizarEnvio` en `FinalizarEnvio.cs`, que
    implementa la interfaz `IFinalizarEnvio`. Incluye el método
    `finalizarEnvio(int idEnvio, DateTime fecha)` con validaciones como:
    verificar ID y fecha válidos, existencia del envío, estado no
    finalizado, y coherencia de fechas. Si las validaciones son correctas,
    se finaliza el envío y se actualiza en el repositorio. Este cambio
    asegura el cumplimiento de las reglas de negocio al finalizar envíos.

---
+++ LogicaAplicacion/CasosUsoConcretos/Envios/FinalizarEnvio.cs
@@ -0,0 +1,56 @@
+﻿using CasosDeUso.InterfacesCasosUso;
+using ExcepcionesPropias;
+using LogicaAccesoADatos.Repos;
+using LogicaNegocio.EntidadesDominio.Envíos;
+using LogicaNegocio.Enums;
+using LogicaNegocio.InterfacesRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUsoConcretos.Envios
+{
+    public class FinalizarEnvio : IFinalizarEnvio
+    {
+        IRepositorioEnvios repoEnvio;
+
+        public FinalizarEnvio(IRepositorioEnvios repoEnvio)
+        {
+            this.repoEnvio = repoEnvio;
+        }
+
+        public void finalizarEnvio(int idEnvio, DateTime fecha)
+        {
+            if (idEnvio <= 0)
+            {
+                throw new DatosInvalidosException("El id del envío no es válido");
+            }
+            if (fecha == null)
+            {
+                throw new DatosInvalidosException("La fecha no es válida");
+            }
+            Envio e = repoEnvio.FindById(idEnvio);
+            if (e == null)
+            {
+                throw new DatosInvalidosException("El envío no existe");
+            }
+            else
+            {
+                if (e.EstadoEnvio == TipoEstadoEnvio.Finalizado)
+                {
+                    throw new DatosInvalidosException("El envío ya fue entregado");
+                }
+                if(e.FechaRegistroEnvio > fecha)
+                {
+                    throw new DatosInvalidosException("La fecha no puede ser anterior a la fecha de registro");
+                }
+                e.finalizarEnvio(fecha);
+                repoEnvio.Update(e);
+            }
+
+        }
+    }
+    
+}
