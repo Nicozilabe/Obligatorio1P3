c878c79bc9ec91b429d3ff04a8a6f5b7f2c2448a
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-05-18 21:02:47 -0300

    Añadir validaciones a ComentarioEnvioDTO y DireccionDTO
    Se añadió la referencia a `System.ComponentModel.DataAnnotations`
    en los archivos `ComentarioEnvioDTO.cs` y `DireccionDTO.cs` para
    habilitar el uso de atributos de validación en las propiedades.
    
    En `ComentarioEnvioDTO.cs`:
    - `[Required]` y `[StringLength(32)]` añadidos a `Comentario`.
    
    En `DireccionDTO.cs`:
    - `[Required]` y `[StringLength(32)]` añadidos a `Calle`.
    - `[Required]`, `[Range(1, 9999)]` añadidos a `Numero`.
    - `[Required]`, `[Range(0, 99999)]` añadidos a `CodigoPostal`.

--- CasosDeUso/DTOs/Envio/ComentarioEnvioDTO.cs
+++ CasosDeUso/DTOs/Envio/ComentarioEnvioDTO.cs
@@ -1,11 +1,14 @@
 ﻿using CasosDeUso.DTOs.Usuarios;
 using CasosDeUso.InterfacesCasosUso;
+using System.ComponentModel.DataAnnotations;
 
 namespace CasosDeUso.DTOs.Envio
 {
     public class ComentarioEnvioDTO:IValidable
     {
         public int Id { get; init; }
+        [Required(ErrorMessage = "El campo es obligatorio.")]
+        [StringLength(32, ErrorMessage = "Comentario no puede superar los 32 caracteres.")]
         public string Comentario { get; set; }
         public DateTime Fecha { get; set; }
         public EmpleadoSeguroDTO Empleado { get; set; }
